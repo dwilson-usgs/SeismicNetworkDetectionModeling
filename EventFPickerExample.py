1efc86580a6d657e4587f519c85d09d9c77c8550
dwilson-usgs <dwilson@usgs.gov>
2019-08-08 11:20:09 -0600

    fixed formatting

--- EventFPickerExample.py
+++ EventFPickerExample.py
@@ -26,67 +26,67 @@ import FilterPicker as fp
 
 # Function to do some plotting of events, the filtered version, and CF's
 def PlotTn(tr,Tlong,domper,stat,mag,Edist):
-        dT=tr.stats.delta
-        nTlong=int(min(np.floor(Tlong/dT),np.floor(len(tr.data)-1)))
-        N = int(np.ceil(np.log2(domper/dT)))+1
-        fig=plt.figure(1,figsize=(12,12))
-        t=np.linspace(0, (tr.stats.npts-1)/ tr.stats.sampling_rate,num=tr.stats.npts)-Tlong*2
-        plt.subplot(np.floor(N/2.)+2,1,1)
-        plt.plot(t,tr,'k')
-        plt.ylabel('raw')
+    dT=tr.stats.delta
+    nTlong=int(min(np.floor(Tlong/dT),np.floor(len(tr.data)-1)))
+    N = int(np.ceil(np.log2(domper/dT)))+1
+    fig=plt.figure(1,figsize=(12,12))
+    t=np.linspace(0, (tr.stats.npts-1)/ tr.stats.sampling_rate,num=tr.stats.npts)-Tlong*2
+    plt.subplot(np.floor(N/2.)+2,1,1)
+    plt.plot(t,tr,'k')
+    plt.ylabel('raw')
+    plt.xlim([-10 , 20])
+    #for n in range(N):
+    n=0
+    nn=1
+    while n <= N:      
+        nn=nn+1
+        plt.subplot(np.floor(N/2.)+2,1,nn)
+        plt.plot(t,fp.DiffFilt(tr,n,Tlong),'k')
+        plt.ylabel('T%i=%2.2fs' % (n, (2.**n)*dT) )
         plt.xlim([-10 , 20])
-        #for n in range(N):
-        n=0
-        nn=1
-        while n <= N:      
-                nn=nn+1
-                plt.subplot(np.floor(N/2.)+2,1,nn)
-                plt.plot(t,fp.DiffFilt(tr,n,Tlong),'k')
-                plt.ylabel('T%i=%2.2fs' % (n, (2.**n)*dT) )
-                plt.xlim([-10 , 20])
-                n=n+2
-        plt.xlabel('Time [s]')
-        plt.suptitle(stat.code + ' m' + str(mag) + ' dist=' + str(Edist)+'km')
-        fig=plt.figure(2,figsize=(12,12))
-        plt.subplot(np.floor(N/2.)+3,1,1)
-        plt.plot(t,tr,'k')
-        plt.ylabel('raw')
-        plt.xlim([-10 , 20])
-        n=0
-        nn=1
-        while n <= N:      
-                nn=nn+1
-                plt.subplot(np.floor(N/2.)+3,1,nn)
-                plt.plot(t,fp.CreateCF(tr,n,Tlong),'k')
-                plt.xlim([-10 , 20])
-                ax=plt.gca()
-                plt.text(-9,max(ax.get_ylim())*.75,'CF T%i=%2.2fs' % (n, (2.**n)*dT) )
-                n=n+2
-        plt.subplot(np.floor(N/2.)+3,1,np.floor(N/2.)+3)
-        plt.plot(t,fp.CreateSummaryCF(tr,Tlong,domper),'k')
+        n=n+2
+    plt.xlabel('Time [s]')
+    plt.suptitle(stat.code + ' m' + str(mag) + ' dist=' + str(Edist)+'km')
+    fig=plt.figure(2,figsize=(12,12))
+    plt.subplot(np.floor(N/2.)+3,1,1)
+    plt.plot(t,tr,'k')
+    plt.ylabel('raw')
+    plt.xlim([-10 , 20])
+    n=0
+    nn=1
+    while n <= N:      
+        nn=nn+1
+        plt.subplot(np.floor(N/2.)+3,1,nn)
+        plt.plot(t,fp.CreateCF(tr,n,Tlong),'k')
         plt.xlim([-10 , 20])
         ax=plt.gca()
-        plt.text(-9,max(ax.get_ylim())*.75,'Summary CF')
-        plt.xlabel('Time [s]')
-        plt.suptitle(stat.code + ' m' + str(mag) + ' dist=' + str(Edist)+'km')
-        fig=plt.figure(3,figsize=(12,12))
-        viridis = cm.get_cmap('viridis', N)
-        y = np.append([np.mean(tr.data[1:nTlong])], tr.data)
-        yp=[0.]
-        for i in range(1,len(tr.data)):
-           yp.append(y[i]-y[i-1])
-        f1, Pxx1 = scisig.periodogram(yp, 1./dT)
-        n=0
-        while n <= N:      
-                f, Pxx = scisig.periodogram(fp.DiffFilt(tr,n,Tlong), 1./dT)
-                Pxdiff=10*np.log10(Pxx)-10*np.log10(Pxx1)
-                plt.semilogx(1./f, Pxdiff,Color=viridis(1.0*n/N))
-                plt.text(50,Pxdiff[1],'T%i=%2.2fs' % (n, (2.**n)*dT), color=viridis(1.0*n/N))
-                n=n+2
-        plt.xlabel('Period [s]')
-        plt.ylabel('dB relative to unfiltered')
-        plt.suptitle(stat.code + ' m' + str(mag) + ' dist=' + str(Edist)+'km')
-        plt.show()
+        plt.text(-9,max(ax.get_ylim())*.75,'CF T%i=%2.2fs' % (n, (2.**n)*dT) )
+        n=n+2
+    plt.subplot(np.floor(N/2.)+3,1,np.floor(N/2.)+3)
+    plt.plot(t,fp.CreateSummaryCF(tr,Tlong,domper),'k')
+    plt.xlim([-10 , 20])
+    ax=plt.gca()
+    plt.text(-9,max(ax.get_ylim())*.75,'Summary CF')
+    plt.xlabel('Time [s]')
+    plt.suptitle(stat.code + ' m' + str(mag) + ' dist=' + str(Edist)+'km')
+    fig=plt.figure(3,figsize=(12,12))
+    viridis = cm.get_cmap('viridis', N)
+    y = np.append([np.mean(tr.data[1:nTlong])], tr.data)
+    yp=[0.]
+    for i in range(1,len(tr.data)):
+        yp.append(y[i]-y[i-1])
+    f1, Pxx1 = scisig.periodogram(yp, 1./dT)
+    n=0
+    while n <= N:      
+        f, Pxx = scisig.periodogram(fp.DiffFilt(tr,n,Tlong), 1./dT)
+        Pxdiff=10*np.log10(Pxx)-10*np.log10(Pxx1)
+        plt.semilogx(1./f, Pxdiff,Color=viridis(1.0*n/N))
+        plt.text(50,Pxdiff[1],'T%i=%2.2fs' % (n, (2.**n)*dT), color=viridis(1.0*n/N))
+        n=n+2
+    plt.xlabel('Period [s]')
+    plt.ylabel('dB relative to unfiltered')
+    plt.suptitle(stat.code + ' m' + str(mag) + ' dist=' + str(Edist)+'km')
+    plt.show()
 
 
 
@@ -122,28 +122,28 @@ inventory = client.get_stations(network=nets,station=stas,channel=chans,starttim
 print(inventory)
 
 for cnet in inventory:
-                for stat in cnet:
-                   print(stat)
-                   for evt in cat:
-                        if 1:
-                           tim=evt.origins[0].time
-                           epi_dist, az, baz = gps2dist_azimuth(evt.origins[0].latitude,evt.origins[0].longitude, stat.latitude, stat.longitude)
-                           epi_dist = epi_dist / 1000
-                           arrivals = model.get_travel_times(source_depth_in_km=evt.origins[0].depth / 1000,
-                                  distance_in_degree=epi_dist/111.1949,
-                                  phase_list=["p","P"])
-                           arrp=arrivals[0]
-                           arrivals = model.get_travel_times(source_depth_in_km=evt.origins[0].depth / 1000,
-                                  distance_in_degree=epi_dist/111.1949,
-                                  phase_list=["s","S"])
-                           arrs=arrivals[0]
-                           print(arrp, arrs)
-                           if epi_dist <= max_epi_dist:     
-                             st = client.get_waveforms(cnet.code, stat.code, "*", chans, tim+arrp.time-Tlong*2, tim+arrs.time+30, attach_response=True)
-                             print(cnet.code, stat.code, chans, tim, len(st))
-                             if debug == True:
-                                PlotTn(st[0],Tlong,domper,stat,evt.magnitudes[0].mag,round(epi_dist))
-                        else:
-                           print("Could not fetch %s-%s %s" % (cnet.code, stat.code, tim))
+    for stat in cnet:
+        print(stat)
+        for evt in cat:
+            try:
+                tim=evt.origins[0].time
+                epi_dist, az, baz = gps2dist_azimuth(evt.origins[0].latitude,evt.origins[0].longitude, stat.latitude, stat.longitude)
+                epi_dist = epi_dist / 1000
+                arrivals = model.get_travel_times(source_depth_in_km=evt.origins[0].depth / 1000,
+                    distance_in_degree=epi_dist/111.1949,
+                    phase_list=["p","P"])
+                arrp=arrivals[0]
+                arrivals = model.get_travel_times(source_depth_in_km=evt.origins[0].depth / 1000,
+                    distance_in_degree=epi_dist/111.1949,
+                    phase_list=["s","S"])
+                arrs=arrivals[0]
+                print(arrp, arrs)
+                if epi_dist <= max_epi_dist:     
+                    st = client.get_waveforms(cnet.code, stat.code, "*", chans, tim+arrp.time-Tlong*2, tim+arrs.time+30, attach_response=True)
+                    print(cnet.code, stat.code, chans, tim, len(st))
+                    if debug == True:
+                        PlotTn(st[0],Tlong,domper,stat,evt.magnitudes[0].mag,round(epi_dist))
+            except:
+                print("Could not fetch %s-%s %s" % (cnet.code, stat.code, tim))
                         
 
--- FilterPicker.py
+++ FilterPicker.py
@@ -15,73 +15,73 @@ import numpy as np
 
 
 def DiffFilt(tr,n,Tlong):
-        """
-        Function to produce filtered differential signals as a function of n
-        according to Lomax et al, 2012, eqns. 2-5.  (note that eqn. 5 in the
-        oridinal paper and was corrected later
-        see:http://alomax.free.fr/FilterPicker/erratum.html)
-        :param tr - an obspy trace object
-        :param n - the band number
-        :param Tlong - the time-averaging scale
-        :return: a filtered differential signal in the form of a obspy trace object
-        """
-        dT=tr.stats.delta
-        nTlong=int(min(np.floor(Tlong/dT),np.floor(len(tr.data)-1)))
-        Tn = (2**n)*dT
-        wn = Tn/(2.*np.pi)
-        y=np.diff(tr.data)
-        CHP=wn/(wn+ dT)
-        CLP = dT/(wn + dT) 
-        y = np.append([np.mean(tr.data[1:nTlong])], tr.data)
-        yp=[0.]
-        for i in range(1,len(tr.data)):
-           yp.append(y[i]-y[i-1])
-        YHP1, YHP2, YLP = [0.], [0.], [0.]
-        for i in range(1,len(yp)):            
-            YHP1.append(CHP*(YHP1[i-1] + yp[i] - yp[i-1]))
-            YHP2.append(CHP*(YHP2[i-1] + YHP1[i] - YHP1[i-1]))
-            #YLP.append(YLP[i-1] + CLP*(YHP2[i] - YHP2[i-1]))
-            #the line above was the original incorect eqn. 5, below is correct.
-            YLP.append(YLP[i-1] + CLP*(YHP2[i] - YLP[i-1]))
-        TRYLP = tr.copy()
-        TRYLP.data=np.array(YLP)
-        return TRYLP
+    """
+    Function to produce filtered differential signals as a function of n
+    according to Lomax et al, 2012, eqns. 2-5.  (note that eqn. 5 in the
+    oridinal paper and was corrected later
+    see:http://alomax.free.fr/FilterPicker/erratum.html)
+    :param tr - an obspy trace object
+    :param n - the band number
+    :param Tlong - the time-averaging scale
+    :return: a filtered differential signal in the form of a obspy trace object
+    """
+    dT=tr.stats.delta
+    nTlong=int(min(np.floor(Tlong/dT),np.floor(len(tr.data)-1)))
+    Tn = (2**n)*dT
+    wn = Tn/(2.*np.pi)
+    y=np.diff(tr.data)
+    CHP=wn/(wn+ dT)
+    CLP = dT/(wn + dT) 
+    y = np.append([np.mean(tr.data[1:nTlong])], tr.data)
+    yp=[0.]
+    for i in range(1,len(tr.data)):
+       yp.append(y[i]-y[i-1])
+    YHP1, YHP2, YLP = [0.], [0.], [0.]
+    for i in range(1,len(yp)):            
+        YHP1.append(CHP*(YHP1[i-1] + yp[i] - yp[i-1]))
+        YHP2.append(CHP*(YHP2[i-1] + YHP1[i] - YHP1[i-1]))
+        #YLP.append(YLP[i-1] + CLP*(YHP2[i] - YHP2[i-1]))
+        #the line above was the original incorect eqn. 5, below is correct.
+        YLP.append(YLP[i-1] + CLP*(YHP2[i] - YLP[i-1]))
+    TRYLP = tr.copy()
+    TRYLP.data=np.array(YLP)
+    return TRYLP
 
 def CreateCF(tr,n,Tlong):
-        """
-        Function to produce a characteristic function (CF)
-        according to Lomax et al, 2012, eqns. 6-7
-        :param tr - an obspy trace object
-        :param n - the band number
-        :param Tlong - the time-averaging scale
-        :return: The CF which is the same length as tr.data
-        """
-        YLP=DiffFilt(tr,n,Tlong)
-        E=np.power(YLP.data,2)
-        dT=YLP.stats.delta
-        nTlong=int(min(np.floor(Tlong/dT),len(YLP.data)-1))
-        CF=[0., 0.]
-        for i in range(2,len(E)):
-                Eave=np.mean(E[int(max(0,i-nTlong)):i])
-                sigma=np.std(E[int(max(0,i-nTlong)):i])
-                CF.append( (E[i] - Eave)/sigma)
-        return CF
+    """
+    Function to produce a characteristic function (CF)
+    according to Lomax et al, 2012, eqns. 6-7
+    :param tr - an obspy trace object
+    :param n - the band number
+    :param Tlong - the time-averaging scale
+    :return: The CF which is the same length as tr.data
+    """
+    YLP=DiffFilt(tr,n,Tlong)
+    E=np.power(YLP.data,2)
+    dT=YLP.stats.delta
+    nTlong=int(min(np.floor(Tlong/dT),len(YLP.data)-1))
+    CF=[0., 0.]
+    for i in range(2,len(E)):
+        Eave=np.mean(E[int(max(0,i-nTlong)):i])
+        sigma=np.std(E[int(max(0,i-nTlong)):i])
+        CF.append( (E[i] - Eave)/sigma)
+    return CF
 
 def CreateSummaryCF(tr,Tlong,domper):
-        """
-        Function to produce a characteristic function (CF)
-        according to Lomax et al, 2012, eqns. 6-7 and in
-        the text after eqn. 7.
-        :param tr - an obspy trace object
-        :param n - the band number
-        :param Tlong - the time-averaging scale
-        :return: The CF which is the same length as tr.data
-        """
-        dT=tr.stats.delta
-        N = int(np.ceil(np.log2(domper/dT)))+1
-        CFmatrix=[]
-        for n in range(N):    
-                CFmatrix.append(CreateCF(tr,n,Tlong))
-        CFsum=np.amax(CFmatrix, axis=0)        
-        return CFsum
+    """
+    Function to produce a characteristic function (CF)
+    according to Lomax et al, 2012, eqns. 6-7 and in
+    the text after eqn. 7.
+    :param tr - an obspy trace object
+    :param n - the band number
+    :param Tlong - the time-averaging scale
+    :return: The CF which is the same length as tr.data
+    """
+    dT=tr.stats.delta
+    N = int(np.ceil(np.log2(domper/dT)))+1
+    CFmatrix=[]
+    for n in range(N):    
+        CFmatrix.append(CreateCF(tr,n,Tlong))
+    CFsum=np.amax(CFmatrix, axis=0)        
+    return CFsum
 
