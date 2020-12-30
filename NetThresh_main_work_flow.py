113b876c84507326eae3fa8239c68998c704cad8
dwilson-usgs <dwilson@usgs.gov>
2020-12-30 12:46:34 -0700

    added modeling scripts

---
+++ NetThresh_main_work_flow.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python
+
+from obspy.clients.fdsn import Client
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.mlab as ml
+from obspy.core import UTCDateTime
+from scipy.interpolate import griddata
+from obspy.geodetics import gps2dist_azimuth
+import matplotlib
+from obspy.taup import TauPyModel
+from scipy import signal as scisig
+from obspy.signal import spectral_estimation as spec
+from matplotlib import cm
+import pickle
+import cartopy.crs as ccrs
+import cartopy
+import cartopy.feature as cfeature
+from matplotlib.patches import Circle
+import matplotlib.ticker as mticker
+import csv
+
+import thresholdmodeling as tm       ### this is the threshold modeling module
+
+model = TauPyModel(model="iasp91")
+client = Client("IRIS")
+
+##########################################################
+################# User input Section #####################
+# time for station noise analysis
+starttime = UTCDateTime("2020-08-01 05:00:00")
+endtime = UTCDateTime("2020-08-01 05:10:00")
+
+# coordinates of study area
+#boxcoords=[38.0, -81.0, 48.0, -66] # new england
+#boxcoords=[34.0, -94.0, 41.0, -83] # new madrid
+#boxcoords=[33.5, -100.1, 37.5, -94.4] # oklahoma
+boxcoords=[31.0, -91.0, 37.0, -83] # for csv test
+
+# in degrees, box coords buffer to select station out side of box
+bb=2 
+# number of stations required for event association
+nsta=5 
+# number of phase picks required for event association
+npick=12 
+# error level for estimating
+velerr=.05
+# magnitude to use for plotting detection distance circles
+cm=2.0
+# Calculate noise levels, or load them from a pickle or csv file?
+calc=False
+pickleorcsv='csv'      #this should be 'csv' or 'pickle', only used if calc=False
+
+# title to be used for saving files
+titl="csvtest" # title to be used for saving and loading files
+
+debug = True
+
+if calc:
+    # build an inventory
+    stas= "*"
+    nets="IU,US,N4,NE,TA,PE,CN,NM,ET,AG,AO"
+    chans="HH*,BH*"
+    #nets="O2,OK,US,N4,TA,GS"
+
+
+
+
+
+    inventory = client.get_stations(network=nets,station=stas,channel=chans,starttime=starttime, endtime=endtime, minlatitude=boxcoords[0]-bb,
+                                        minlongitude=boxcoords[1]-bb, maxlatitude=boxcoords[2]+bb, maxlongitude=boxcoords[3]+bb, level='response')
+
+
+    # if you have other sites to add that can't be easily wildcarded, you can add them like this
+    othernets="LD"
+    othersites="NCB,FLET,KSCT,MCVT,HCNY,NPNY,PAL,ODNJ,WCNY,WVNY,SDMD,GCMD,GEDE,WADE"
+    #othersites="BLO,BVIL,CBMO,CGM3,CGMO,EDIL,EVIN,FFIL,FVM,GBIN,HAIL,JCMO,MCIL,MGMO,MPH,MVKY,OHIN,OLIL,PBMO,PIOH,PLAL,PVMO,SCIN,SCMO,SIUC,STIL,TCIN,TYMO,UALR,USIN,UTMT,WVIL"
+
+    inventory += client.get_stations(network=othernets,station=othersites,channel=chans,starttime=starttime,
+                                        endtime=endtime,  level='response')
+
+####     end of user input   ####################
+#############################################################################   
+######### parameters below are default filter parameters for P and S ########
+fmin=1.25
+fmax=20.
+fminS=0.8
+fmaxS=12.5
+
+# Median P and S values from noise study
+PdBval=-114.4
+SdBval=-114.6
+
+Pstd=10**(PdBval/20)
+Sstd=10**(SdBval/20)
+
+if calc==True:
+    Sdict=tm.calc_noise(inventory,starttime, endtime, fmin, fmax, fminS,fmaxS)
+                            
+    with open('NoiseVals%s%s.pickle'%(titl,starttime.strftime('%Y%m%d%H')),'wb') as f:
+        pickle.dump([Sdict, boxcoords],f)
+    f.close()
+else:
+    if pickleorcsv=='pickle':
+        with open('NoiseVals%s%s.pickle'%(titl,starttime.strftime('%Y%m%d%H')),'rb') as f:
+            Sdict, boxcoords = pickle.load(f)
+        f.close()
+    elif pickleorcsv=='csv':
+        Sdict=tm.calc_noise_csv('NoiseVals%s%s.csv'%(titl,starttime.strftime('%Y%m%d%H')))
+
+    else:
+        print("pickleorcsv must be set to either 'pickle' or 'csv'. ")
+        
+ltmin=np.floor(boxcoords[0]/2)*2
+ltmax=np.ceil(boxcoords[2]/2)*2 +2
+lnmin=np.floor(boxcoords[1]/2)*2
+lnmax=np.ceil(boxcoords[3]/2)*2 +2
+
+for sta in Sdict:
+    Sdict[sta]['skip']=0
+    Sdict[sta]['hit']=0
+
+# set up a grid for modelling
+x=np.arange(boxcoords[1],boxcoords[3]+.1,.1)
+y=np.arange(boxcoords[0],boxcoords[2]+.1,.1)
+
+results, Sdict = tm.model_thresh(Sdict,x,y,npick,velerr,dist_cut=250)
+
+x=np.asarray(results[:,0])
+y=np.asarray(results[:,1])
+z=np.asarray(results[:,2])
+zd=np.asarray(results[:,4])
+zd2=np.asarray(results[:,6])
+ze=np.asarray(results[:,7])
+
+extent=[boxcoords[1], boxcoords[3], boxcoords[0], boxcoords[2]]
+central_lon = np.mean(extent[:2])
+central_lat = np.mean(extent[2:])
+nbins=300
+xi, yi = np.mgrid[x.min():x.max():nbins*1j, y.min():y.max():nbins*1j]
+
+zi = griddata( (x,y), z, (xi, yi), method='cubic')
+zdi = griddata( (x,y), zd, (xi, yi), method='cubic')
+zd2i = griddata( (x,y), zd2, (xi, yi), method='linear')
+zei = griddata( (x,y), ze, (xi, yi), method='cubic')
+
+#with open('DetectGrid%s%s.pickle'%(titl,starttime.strftime('%Y%m%d%H')),'wb') as f:
+#    pickle.dump([xi,yi,zi],f)
+#f.close()
+    
+if 1:
+    plt.figure(7, figsize=(10,6))
+    c1=np.floor(min(z)*10)/10
+    c2=np.ceil(max(z)*10)/10
+    #c1=1.0
+    #c2=2.9
+    #ax = plt.axes(projection=ccrs.AlbersEqualArea(central_lon, central_lat))
+    ax = plt.axes(projection=ccrs.Mercator(central_lon))
+    ax.set_extent(extent)
+
+    ax.add_feature(cartopy.feature.OCEAN)
+    ax.add_feature(cartopy.feature.LAND, edgecolor='black')
+    ax.add_feature(cartopy.feature.LAKES, edgecolor='black')
+    ax.add_feature(cartopy.feature.STATES, edgecolor='black')
+
+    matplotlib.rcParams['xtick.direction'] = 'out'
+    matplotlib.rcParams['ytick.direction'] = 'out'
+
+    plt.contourf(xi, yi, zi.reshape(xi.shape), np.arange(c1, c2+.01, 0.1), cmap=plt.cm.plasma, transform=ccrs.PlateCarree() )
+    gridlines=ax.gridlines(draw_labels=True, color='gray', alpha=.8, linestyle=':')
+    gridlines.xlabels_top=False
+    gridlines.ylabels_right=False
+    gridlines.xlocator = mticker.FixedLocator(np.arange(lnmin,lnmax,2))
+    gridlines.ylocator = mticker.FixedLocator(np.arange(ltmin,ltmax,2))
+
+    # Add color bar
+    plt.clim(c1,c2)
+    cbar=plt.colorbar()
+    cbar.set_label('Detection Threshold (M)')
+
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    #plt.title("%s - %s "% (letter.upper(), content.upper()))
+    for sta in Sdict:
+        plt.plot(Sdict[sta]['lon'],Sdict[sta]['lat'], 'kd', markersize=4.5, transform=ccrs.PlateCarree())
+
+if 1:
+    plt.figure(2, figsize=(10,7))
+    #ax3 = plt.axes(projection=ccrs.AlbersEqualArea(central_lon, central_lat))
+    ax3 = plt.axes(projection=ccrs.Mercator(central_lon))
+    ax3.set_extent(extent)
+
+    ax3.add_feature(cartopy.feature.OCEAN)
+    ax3.add_feature(cartopy.feature.LAND, edgecolor='black')
+    ax3.add_feature(cartopy.feature.LAKES, edgecolor='black')
+    ax3.add_feature(cartopy.feature.STATES, edgecolor='black')
+    plt.contourf(xi, yi, zdi.reshape(xi.shape), cmap=plt.cm.plasma, transform=ccrs.PlateCarree() )
+    # Add color bar
+    gridlines=ax3.gridlines(draw_labels=True, color='gray', alpha=.8, linestyle=':')
+    gridlines.xlabels_top=False
+    gridlines.ylabels_right=False
+    gridlines.xlocator = mticker.FixedLocator(np.arange(lnmin,lnmax,2))
+    gridlines.ylocator = mticker.FixedLocator(np.arange(ltmin,ltmax,2))
+    #plt.clim(0.0,300)
+    cbar=plt.colorbar()
+    cbar.set_label('Distance to %ith nearest pick'%(npick))
+
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    #plt.title("%s - %s "% (letter.upper(), content.upper()))
+    for sta in Sdict:
+        plt.plot(Sdict[sta]['lon'],Sdict[sta]['lat'], 'kd', markersize=4.5, transform=ccrs.PlateCarree())
+
+if 1:
+    plt.figure(3, figsize=(10,7))
+    #ax3 = plt.axes(projection=ccrs.AlbersEqualArea(central_lon, central_lat))
+    ax3 = plt.axes(projection=ccrs.Mercator(central_lon))
+    ax3.set_extent(extent)
+
+    ax3.add_feature(cartopy.feature.OCEAN)
+    ax3.add_feature(cartopy.feature.LAND, edgecolor='black')
+    ax3.add_feature(cartopy.feature.LAKES, edgecolor='black')
+    ax3.add_feature(cartopy.feature.STATES, edgecolor='black')
+    zd2i=zd2i+.0001
+    plt.contourf(xi, yi, zd2i.reshape(xi.shape), np.arange(1.0, 3.2, 0.2),cmap=plt.cm.plasma, transform=ccrs.PlateCarree() )
+    # Add color bar
+    gridlines=ax3.gridlines(draw_labels=True, color='gray', alpha=.8, linestyle=':')
+    gridlines.xlabels_top=False
+    gridlines.ylabels_right=False
+    gridlines.xlocator = mticker.FixedLocator(np.arange(lnmin,lnmax,2))
+    gridlines.ylocator = mticker.FixedLocator(np.arange(ltmin,ltmax,2))
+    #plt.clim(0.0,2)
+    cbar=plt.colorbar()
+    #plt.clim(1,2.5)
+    cbar.set_label('Distance to %ith nearest pick (actual / possible)'%(npick))
+
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    #plt.title("%s - %s "% (letter.upper(), content.upper()))
+    for sta in Sdict:
+        plt.plot(Sdict[sta]['lon'],Sdict[sta]['lat'], 'kd', markersize=4.5, transform=ccrs.PlateCarree())
+
+#lfeat=cartopy.feature.NaturalEarthFeature(category='physical',name='land',scale='50m',facecolor='none')
+#ofeat=cartopy.feature.NaturalEarthFeature(category='physical',name='ocean',scale='50m',facecolor='none')
+if 1:
+    f=open('NoiseCircleVals%s%s.csv'%(titl,starttime.strftime('%Y%m%d%H')),"w+")
+    f.write("station, p-dist, s-dist , effectiveness (%), used, total \n")
+    plt.figure(1, figsize=(10,8))
+    #ax2 = plt.axes(projection=ccrs.AlbersEqualArea(central_lon, central_lat))
+    ax2 = plt.axes(projection=ccrs.Mercator(central_lon))
+    ax2.set_extent(extent)
+    ax2.add_feature(cartopy.feature.OCEAN)
+    ax2.add_feature(cartopy.feature.LAND, edgecolor='black')
+    ax2.add_feature(cartopy.feature.LAKES, edgecolor='black')
+    ax2.add_feature(cartopy.feature.STATES, edgecolor='black')
+    for sta in Sdict:
+        tot=Sdict[sta]['hit']+Sdict[sta]['skip']
+        if tot >0.1:
+            eff=100*Sdict[sta]['hit'] / (tot)
+        else:
+            eff=0
+        plt.plot(Sdict[sta]['lon'],Sdict[sta]['lat'], 'kd', markersize=4.5, transform=ccrs.PlateCarree())
+        if Sdict[sta]['chans']['V']:
+            PdB=20*np.log10(Sdict[sta]['chans']['V'])
+            #m25d=np.exp((2.2-aP[0]-aP[1]*PdB)/aP[2])
+            m25d=tm.calc_magdist(PdB,cm,phase='P')
+            circle=Circle((Sdict[sta]['lon'],Sdict[sta]['lat']),(m25d)/111.1949,color='blue',alpha=.2,transform=ccrs.Geodetic())
+            ax2.add_patch(circle)
+        if Sdict[sta]['chans']['H']:
+            SdB=20*np.log10(Sdict[sta]['chans']['H'])
+            #m25dS=np.exp((2.2-aS[0]-aS[1]*SdB)/aS[2])
+            m25dS=tm.calc_magdist(SdB,cm,phase='S')
+            circleS=Circle((Sdict[sta]['lon'],Sdict[sta]['lat']),(m25dS)/111.1949,color='green',alpha=.2,transform=ccrs.Geodetic())
+            ax2.add_patch(circleS)
+        if Sdict[sta]['lon'] > extent[0] and Sdict[sta]['lon'] < extent[1] and Sdict[sta]['lat'] > extent[2] and Sdict[sta]['lat'] < extent[3]:
+            plt.text(Sdict[sta]['lon']+.05,Sdict[sta]['lat']+.05, sta,transform=ccrs.Geodetic())
+            if Sdict[sta]['chans']['V'] and Sdict[sta]['chans']['H']:
+                f.write("%s, %4.1f, %4.1f , %4.1f, %i, %i \n"%(sta,m25d,m25dS,eff, Sdict[sta]['hit'],tot))
+            elif Sdict[sta]['chans']['V']:
+                f.write("%s, %4.1f, X, %4.1f, %i, %i \n"%(sta,m25d,eff, Sdict[sta]['hit'],tot))
+    #plt.plot(slons,slats, 'kd', markersize=4.5, transform=ccrs.PlateCarree())
+    gridlines=ax2.gridlines(draw_labels=True, color='gray', alpha=.8, linestyle=':')
+    gridlines.xlabels_top=False
+    gridlines.ylabels_right=False
+    gridlines.xlocator = mticker.FixedLocator(np.arange(lnmin,lnmax,2))
+    gridlines.ylocator = mticker.FixedLocator(np.arange(ltmin,ltmax,2))
+    f.close()
+    #plt.show()
+
+#print('station ,     effectiveness (%)')
+#for sta in Sdict:
+#    tot=Sdict[sta]['hit']+Sdict[sta]['skip']
+#    if tot >0.1:
+#        eff=100*Sdict[sta]['hit'] / (tot)
+#    else:
+#        eff=0
+#    print(sta + ",    %4.1f (%i / %i)" % (eff, Sdict[sta]['hit'],tot))
+
+if 1:
+    plt.figure(8, figsize=(10,6))
+    #ax3 = plt.axes(projection=ccrs.AlbersEqualArea(central_lon, central_lat))
+    ax3 = plt.axes(projection=ccrs.Mercator(central_lon))
+    ax3.set_extent(extent)
+
+    ax3.add_feature(cartopy.feature.OCEAN)
+    ax3.add_feature(cartopy.feature.LAND, edgecolor='black')
+    ax3.add_feature(cartopy.feature.LAKES, edgecolor='black')
+    ax3.add_feature(cartopy.feature.STATES, edgecolor='black')
+    plt.contourf(xi, yi, zei.reshape(xi.shape), cmap=plt.cm.plasma, transform=ccrs.PlateCarree() )
+    # Add color bar
+    gridlines=ax3.gridlines(draw_labels=True, color='gray', alpha=.8, linestyle=':')
+    gridlines.xlabels_top=False
+    gridlines.ylabels_right=False
+    gridlines.xlocator = mticker.FixedLocator(np.arange(lnmin,lnmax,2))
+    gridlines.ylocator = mticker.FixedLocator(np.arange(ltmin,ltmax,2))
+    #plt.clim(0.0,300)
+    cbar=plt.colorbar()
+    cbar.set_label('Horizontal location error (km)')
+
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    #plt.title("%s - %s "% (letter.upper(), content.upper()))
+    for sta in Sdict:
+        plt.plot(Sdict[sta]['lon'],Sdict[sta]['lat'], 'kd', markersize=4.5, transform=ccrs.PlateCarree())
+    plt.show()
+
+    
---
+++ thresholdmodeling.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+
+import numpy as np
+import collections
+from obspy.clients.fdsn import Client
+
+from obspy.geodetics import gps2dist_azimuth
+from obspy.taup import TauPyModel
+model = TauPyModel(model="iasp91")
+client = Client("IRIS")
+import csv
+
+#from numpy import log10 as l10
+#from numpy import where as npwh
+
+def calc_model(x,y,coeffs='CEUS',phase='P'):
+    """
+    Returns the minimum detectable magnitude for
+    noise level x (in dB) and distance y (in km).
+    If x or y are vectors, they must be the same length.
+    coeffs specifies which model coefficients to use (only option is 'CEUS')
+    """
+    a=get_coeffs(coeffs,phase)
+    model=[]
+    if np.size(y) > 1:
+        for n in range(len(y)):
+            model.append(calc_model_hinged(a,x[n],y[n]))
+    else:
+        model.append(calc_model_hinged(a,x,y))
+
+    return np.asarray(model)
+
+def calc_model_hinged(a,x,y):
+    #order of coefficients in a
+    #  0, 1,  2,  3,  4,  5,  6,  7,  8, 9
+    # R1, R2, c, a1, a2, a3, b1, b2, b3, d
+    if y <= a[0]:
+        model=(a[2]+a[3]*np.log10(y)+ a[6]*y +a[8]*(x/20))
+    elif y >= a[1]:
+        model=(a[2]+a[5]*np.log10(y/a[1])+a[4]*np.log10(a[1]/a[0]) +a[3]*np.log10(a[0]) + +a[7]*(a[1]-a[0]) + a[6]*a[0]+a[8]*(x/20))
+    else:
+        model=(a[2]+a[4]*np.log10(y/a[0])+a[3]*np.log10(a[0])+a[7]*(y-a[0])  +a[6]*a[0]+a[8]*(x/20))
+    return model
+
+def calc_magdist(x,m,coeffs='CEUS',phase='P',xmax=1000):
+    """
+    returns the max distance (in km) at which a magnitude m event
+    can be automatically picked at a station with noise
+    level x (in dB).  xmax is the max distance to check (in km).
+    coeffs specifies which model coefficients to use (only option is 'CEUS')
+    """
+    #a=get_coeffs(coeffs)
+    y=np.arange(1.,xmax,1.)
+    xx=y*0 + x
+    model=calc_model(xx,y,coeffs,phase)
+    indx0=np.where(model<=m)
+    return y[indx0[-1][-1]]
+
+def get_coeffs(coeffs='CEUS',phase='P'):
+    """
+    returns the model cofficients for the specified model and phase pair.
+    Options are 'CEUS' for model and 'P' or 'S' for phase.
+    """
+    if coeffs=='CEUS' and phase == 'P':
+        a=[  1.06388089e+02,   2.95649447e+02,  -2.83799301e-01,
+         3.65990396e+00,  -6.73726922e+00,   1.52716745e+00,
+        -5.29509900e-03,   2.09248651e-02,   6.85527053e-01]
+    elif coeffs=='CEUS' and phase == 'S':
+        #a=[  1.09921161e+02,   2.42091508e+02,   4.48352320e+00,
+        #-8.42355568e-01,  -2.24932817e+01,   1.77700026e+00,
+         #1.92439289e-02,   6.21760423e-02,   4.12947199e-01]
+        a=[  1.05904091e+02,   2.33302291e+02,   4.54873180e+00,
+        -6.22339193e-01,  -2.11649770e+01,   1.60184414e+00,
+         1.96123207e-02,   6.22953721e-02,   5.06311904e-01]
+    else:
+        a=0
+        print('no such coefficient and phase pair')
+    return a
+
+def calc_noise(inventory,starttime, endtime, fmin=1.25, fmax=25., fminS=0.8,fmaxS=12.5):
+    """
+    calc_noise(inventory,starttime, endtime, fmin=1.25, fmax=25., fminS=0.8,fmaxS=12.5)
+    
+    Returns a station dictionary that has station info and noise values computed
+    from data fetched from IRIS.
+    """
+    Sdict = collections.defaultdict(dict)
+    stnum=-1
+    for cnet in inventory:
+        for sta in cnet:
+    #        stnum=stnum+1
+            hcount=0
+            hsum=0
+            if not Sdict[sta.code]:
+                Sdict[sta.code] = collections.defaultdict(dict)
+            Sdict[sta.code]['netsta']="%s-%s" % (cnet.code, sta.code)
+            Sdict[sta.code]['lat']=sta.latitude
+            Sdict[sta.code]['lon']=sta.longitude
+            for chan in sta:
+                print("Working on %s-%s-%s" % (cnet.code, sta.code,chan.code))
+                if not Sdict[sta.code]['chans']:
+                        Sdict[sta.code]['chans'] = collections.defaultdict(dict)
+                if np.abs(chan.dip) > 45:
+                    try:
+                        st = client.get_waveforms(cnet.code, sta.code, chan.location_code, chan.code, starttime, endtime, attach_response=True)
+                        st.remove_response(output="ACC")
+                        st2=st.copy()
+                        st2.filter('bandpass', freqmin=1/8, freqmax=1/4, corners=2)
+                        st.filter('bandpass', freqmin=fmin, freqmax=fmax, corners=2)
+                        #print(st)
+                        if 20*np.log10(np.std(st2[0].data)*2)>-150:
+                            if not Sdict[sta.code]['chans']['V']:
+                                Sdict[sta.code]['chans']['V'] = np.std(st[0].data)
+                        else:
+                            print("Possible dead channel %s-%s-%s" % (cnet.code, sta.code,chan.code))
+                    except:
+                        print("Could not fetch %s-%s-%s" % (cnet.code, sta.code,chan.code))
+                if np.abs(chan.dip) < 1.0:
+                    try:
+                        st = client.get_waveforms(cnet.code, sta.code, chan.location_code, chan.code, starttime, endtime, attach_response=True)
+                        st.remove_response(output="ACC")
+                        st2=st.copy()
+                        st2.filter('bandpass', freqmin=1/8, freqmax=1/4, corners=2)
+                        st.filter('bandpass', freqmin=fminS, freqmax=fmaxS, corners=2)
+                        #print(st)
+                        if 20*np.log10(np.std(st2[0].data)*2)>-150:
+                            hcount +=1
+                            hsum += np.std(st[0].data)
+                        else:
+                            print("Possible dead channel %s-%s-%s" % (cnet.code, sta.code,chan.code))
+                    except:
+                        print("Could not fetch %s-%s-%s" % (cnet.code, sta.code,chan.code))   
+            if hcount>0:
+                if not Sdict[sta.code]['chans']['H']:
+                            Sdict[sta.code]['chans']['H'] = hsum/hcount
+    return Sdict
+
+def calc_noise_csv(csvfile):
+    """
+    calc_noise_csv(csvfile)
+    
+    Returns a station dictionary that has station info and noise values read
+    in from a csv file. csv file format shoult be
+    Net, Station, Lat, Lon, V or H, noiseval
+
+    V or H indicates if it is a vertical of horizontal channel.
+    If more that one H channel is listed for a station, the mean H value will be used.
+    noiseval is the std of the time domain acceleration data, units are m/s/s.
+    """
+    Sdict = collections.defaultdict(dict)
+    stnum=-1
+    with open(csvfile) as csvf:
+        readCSV = csv.reader(csvf, delimiter=',')
+        for row in readCSV:
+            if not Sdict[row[1]]:
+                Sdict[row[1]] = collections.defaultdict(dict)
+                Sdict[row[1]]['netsta']="%s-%s" % (row[0],row[1])
+                Sdict[row[1]]['lat']=np.float(row[2])
+                Sdict[row[1]]['lon']=np.float(row[3])
+                Sdict[row[1]]['chans'] = collections.defaultdict(dict)
+            if row[4] == 'V' and not Sdict[row[1]]['chans']['V']:
+                Sdict[row[1]]['chans']['V'] = np.float(row[5])
+            if row[4] == 'H' and not Sdict[row[1]]['chans']['H']:
+                Sdict[row[1]]['chans']['H'] = np.float(row[5])
+            elif row[4] == 'H':
+                Sdict[row[1]]['chans']['H'] = (Sdict[row[1]]['chans']['H'] +np.float(row[5]))/2
+    return Sdict
+
+def model_thresh(Sdict,x,y,npick,velerr,dist_cut=250,coeffs='CEUS'):
+    results=[]
+    lnx=0
+    for xi in x:
+        lnx += 1
+        
+        for yi in y:
+            stat_results=[]
+            dists=[]
+            for sta in Sdict:
+                stamag=9.0
+                epi_dist, az, baz = gps2dist_azimuth(yi,xi,Sdict[sta]['lat'],Sdict[sta]['lon'])
+                ya=epi_dist / 1000
+                for chan in Sdict[sta]['chans']:
+                    xa=np.log10(Sdict[sta]['chans'][chan])*20.
+                    if 'V' in chan:
+                        model=calc_model(xa,ya,coeffs,phase='P')
+                        stamag=np.min([stamag,np.float(model)])
+                    elif 'H' in chan:
+                        model=calc_model(xa,ya,coeffs,phase='S')
+                        stamag=np.min([stamag,np.float(model)])
+                    stat_results.append(model[0])
+                    dists.append(epi_dist / 1000)
+                Sdict[sta]['Msta']=np.float(stamag)
+                Sdict[sta]['edist']=ya
+            if np.min(dists) < dist_cut:
+                imags=np.argsort(stat_results)
+                magdist=0
+                errtot=0
+                errnum=0
+                for ii in imags[0:(npick)]:
+                    magdist=np.max([magdist,dists[ii]])
+                    errnum+=1
+                    errtot+=(dists[ii]*velerr)**2
+                errtot=errtot/errnum
+                errtot=np.sqrt(errtot)
+                #print("magdist: %3.1f, pick_dists: "%(magdist) + str(dists[imags[0]]))
+                dists=np.sort(dists)
+                stat_results=np.sort(stat_results)
+                results.append([xi, yi, stat_results[npick-1], np.min(dists), dists[npick-1], magdist, magdist/dists[npick-1],errtot])
+                for sta in Sdict:
+                    if Sdict[sta]['edist'] <= magdist:
+                        if Sdict[sta]['Msta'] >  stat_results[npick-1]:
+                            Sdict[sta]['skip'] = Sdict[sta]['skip'] + 1
+                        else:
+                            Sdict[sta]['hit'] = Sdict[sta]['hit'] + 1
+                        
+                #print(xi, yi, stat_results[nsta-1])
+        print('%3.1f %% done'%(100*lnx/len(x)))
+    results=np.asarray(results)
+    return results, Sdict
